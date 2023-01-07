2365960618c189727384849660aa9a9b9cc4ff98
t-nishimura <t-nishimura2022@gmail.com>
2023-01-07 04:21:14 +0000

    ADD GRADLE

---
+++ src/main/java/com/example1/camelSample/CamelSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example1.camelSample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamelSampleApplication.class, args);
+	}
+
+}
